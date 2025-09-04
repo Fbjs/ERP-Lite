e6358ed37c41f48d9d741e27b5d06d286b678039
Francisco Ibaceta <fibaceta.arce@gmail.com>
2025-09-04 18:03:42 +0000

    quiero probar con una imagen mas tecnologica o innovadora para el sistem

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 export default function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="/logo.png"
+      src="/logo-dark.png"
       alt="Vollkorn Logo"
       width={120}
       height={60}
